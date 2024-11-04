c00cbf42eb0c8fa147ebd6bbe5380514b4d8e40a
akanksh5 <akankshjagadish@gmail.com>
2024-11-03 23:24:54 -0800

    Added styling to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './Header.css';
 
 function Header() {
   return (
-    <header>
-      <nav>
-        <ul>
+    <header className="header">
+      <div className="logo">
+        <a href="#about">My Portfolio</a> {/* Logo or Name */}
+      </div>
+      <nav className="nav">
+        <ul className="nav-list">
           <li><a href="#about">About</a></li>
           <li><a href="#projects">Projects</a></li>
           <li><a href="#skills">Skills</a></li>
